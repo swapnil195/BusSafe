647fba287edd471007dd4dc3d28a96a9e69c7355
swapnil195 <136228808+swapnil195@users.noreply.github.com>
2025-01-16 21:19:29 +0530

    Add files via upload

---
+++ profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class profile extends StatelessWidget {
+   profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+
+      body: Center(
+        child: Text("profile page"),
+      ),
+    );
+  }
+}
